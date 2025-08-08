1388b3974d0e82098081699ac7576a240e6c5434
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-08 22:01:23 +0530

    20889

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Menu, Order
+from .models import Menu, Order, UserProfile
 
 admin.site.register(Menu)
 admin.site.register(Order)
 # Register your models here.
+admin.site.register(UserProfile)
  python manage.py runserver
