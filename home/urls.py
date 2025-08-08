eb7cd578da4ae99b0b4bfdc534e6e093add5d862
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-08 22:30:17 +0530

    20893

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import menu_list, home
 
 urlpatterns = [
     path('', home, name='home'), 
+    path('api/menu/', menu_list, name='menu-list'),
 ]
