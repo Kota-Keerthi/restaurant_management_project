f953f7afeca48b8d04aee081f566fec0f84370f1
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-09 23:17:21 +0530

    20895

--- home/views.py
+++ home/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+from django.conf import settings
+from django.shortcuts import render
+
+def home(request):
+    context = {
+        "restaurant_name": settings.restaurant_name,
+        "restaurant_address": settings.restaurant_address
+    }
+    return render(request, "home.html", context)
+    
