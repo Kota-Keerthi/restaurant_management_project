169e5f1b77187cb507c0488e0b2f308fa56940b5
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-11 22:00:13 +0530

    20898

--- home/models.py
+++ home/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfile(models.Model):
-    user = models.OneTOOneField(User, on delete=models.CASCADE)
+class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.name
         
-    python manage.py makemigrations
-    python manage.py migrate    
