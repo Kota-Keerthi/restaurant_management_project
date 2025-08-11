03b55d088ad913cccc27deb40eb76cccfc85077f
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-11 22:03:56 +0530

    20898

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Restaurant
 # Create your views here.
+
+def home(request):
+    restaurant = Restaurant.objects.first()
+    return render(request, 'home.html', {
+        'restaurant_name': restaurant.name if restaurant else "Our Restaurant"
+    })
