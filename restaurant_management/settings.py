b6a1ab0f95f366cf7270e2821fccfa52e4a5ebc8
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-11 21:51:30 +0530

    20898

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -131,6 +131,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Restaurnat Info
-Restaurnat_Name = "The Spice Garden"
+Restaurnat_Name = "Tasty Bites"
 Restaurnat_Address = "123 Main Street, Cityville, Country"
 
