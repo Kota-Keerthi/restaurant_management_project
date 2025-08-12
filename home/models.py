4da9a2b34e201cd0823892518ca137d8397d3725
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-12 22:20:00 +0530

    20894

--- home/models.py
+++ home/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.email}"
         
