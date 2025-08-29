085b003a6ae2977c18c1fe102b54b716734b0800
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-29 21:40:13 +0530

    39234

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import ContactMessageForm
+from .forms import ContactForm
 # Create your views here.
 
-def home(request):
+def contact_view(request):
     if request.merhod == 'POST':
         form = ContactMessageForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('home')
+    
+            return HttpResponse("<h3>Thank you for contacting us!</h3>")
     else:
-        form = ContactMessageForm()        
-    return render(request, 'home.html', {'form': form})
+        form = ContactForm()        
+    return render(request, "contact.html", {'form': form})
     })
