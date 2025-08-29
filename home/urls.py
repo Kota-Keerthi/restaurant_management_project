67dea6e444c45a5c287246d6318f9427d1bf38e5
KEERTHI KOTA <159996214+Kota-Keerthi@users.noreply.github.com>
2025-08-29 22:04:14 +0530

    39234

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .import views 
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path("admin/", admin.site.urls),
+    path("account/", include("account.urls")),
+    path("", include("home.urls")),
+    path("orders/", include("orders.urls")),
+    path("products/", include("products.urls")), 
 ]
